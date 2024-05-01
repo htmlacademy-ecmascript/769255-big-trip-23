f00210aac31097aa0e3cd5217aaa0d714ffd3d8a
Inna <khagleeva_inna@mail.ru>
2024-05-01 22:44:19 +0300

    1.23. Начинаем программировать

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+﻿const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js', // Точка входа
+  output: {
+    filename: 'bundle.[contenthash].js', // Имя бандла
+    path: path.resolve(__dirname, 'build'), // Директория для файлов сборки
+    clean: true, // Удаляем предыдущую сборку перед созданием новой
+  },
+  devtool: 'source-map', // Генерируем карту исходного кода
+  plugins: [ // Подключаем плагины
+    new CopyPlugin({ //Плагин для Webpack, который за нас будет копировать файлы из /public в /build и туда же подкладывать бандл
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
+    }),
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //webpack возьмет все js-файлы и прогонит через babel-loader, используя пресет @babel/preset-env
+        exclude: /(node_modules)/, //исключены файлы из node-modules
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          },
+        },
+      },
+    ],
+  },
+};
