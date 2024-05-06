4ea7ff40f2314710e8675e924e8d67ce8dd16d90
Inna <khagleeva_inna@mail.ru>
2024-05-06 16:16:20 +0300

    Разделила на компоненты. Добавила презентер.

---
+++ src/presenter/trip-presenter.js
@@ -0,0 +1,25 @@
+﻿import {render} from '../render';
+import SortingVew from '../view/sorting-view';
+import AddEventView from '../view/add-event-view';
+import EditEventView from '../view/edit-event-view';
+import TripView from '../view/trip-view';
+import EventView from '../view/event-view';
+
+export default class TripPresenter {
+  tripView = new TripView();
+
+  constructor({tripContainer}) {
+    this.tripContainer = tripContainer;
+  }
+
+  init() {
+    render(new SortingVew(), this.tripContainer);
+    render(this.tripView, this.tripContainer);
+    render(new EditEventView(), this.tripView.getElement());
+    render(new AddEventView(), this.tripView.getElement());
+
+    for(let i = 0; i < 3; i++) {
+      render(new EventView(), this.tripView.getElement());
+    }
+  }
+}
---
+++ src/view/trip-view.js
@@ -0,0 +1,22 @@
+﻿import {createElement} from '../render';
+
+function createTripTemplate() {
+  return `<ul class="trip-events__list"></ul>`
+}
+
+export default class TripView {
+  getTemplate() {
+    return createTripTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
